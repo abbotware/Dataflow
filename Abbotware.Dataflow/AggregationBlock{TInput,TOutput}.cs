2b3d077a3acc9a81c8f1a5e94ceea442243b3dee
Abbotware <abbotware@users.noreply.github.com>
2019-11-26 09:25:37 -0500

    sample files
    initial sample

---
+++ Abbotware.Dataflow/AggregationBlock{TInput,TOutput}.cs
@@ -0,0 +1,48 @@
+﻿namespace Abbotware.Dataflow
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Threading.Tasks.Dataflow;
+
+    public class AggregationBlock<TInput, TOutput> : ITargetBlock<TInput>
+    {
+        private readonly ITargetBlock<TInput> consumer;
+
+        private readonly ITargetBlock<TOutput> target;
+
+        public AggregationBlock(Action<TInput> input, Func<TOutput> output, ITargetBlock<TOutput> target)
+            : this((x) => { input(x); return Task.CompletedTask; }, () => { return Task.FromResult(output()); }, target)
+        {
+        }
+
+        public AggregationBlock(Func<TInput, Task> input, Func<Task<TOutput>> output, ITargetBlock<TOutput> target)
+        {
+            this.target = target;
+
+            var edbo = new ExecutionDataflowBlockOptions
+            {
+                BoundedCapacity = 1,
+                MaxDegreeOfParallelism = 1,
+            };
+
+            consumer = new ActionBlock<TInput>(input, edbo);
+
+            var t = consumer.Completion.ContinueWith(async (x) =>
+            {
+                await target.SendAsync(await output());
+                target.Complete();
+            });
+        }
+
+        public Task Completion => consumer.Completion;
+
+        public void Complete() => consumer.Complete();
+
+        void IDataflowBlock.Fault(Exception exception) => consumer.Fault(exception);
+
+        DataflowMessageStatus ITargetBlock<TInput>.OfferMessage(DataflowMessageHeader messageHeader, TInput messageValue, ISourceBlock<TInput> source, bool consumeToAccept)
+        {
+            return consumer.OfferMessage(messageHeader, messageValue, source, consumeToAccept);
+        }
+    }
+}
---
+++ Abbotware.Dataflow/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Abbotware.Dataflow
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Threading.Tasks.Dataflow;
+
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            var sum = 0;
+
+            var edbo = new ExecutionDataflowBlockOptions
+            {
+                BoundedCapacity = 1,
+                MaxDegreeOfParallelism = 1,
+            };
+
+            var lo = new DataflowLinkOptions
+            {
+                PropagateCompletion = true,
+            };
+
+            var third = new TransformBlock<int, string>(x =>
+            {
+                return x.ToString();
+            }, edbo);
+
+
+            // ctor requirement is not desirable, but 'works'
+            var second = new AggregationBlock<int, int>(x => Interlocked.Add(ref sum, x), () => sum, third);
+
+            var first = new TransformBlock<int, int>(x => x + 2, edbo);
+
+            first.LinkTo(second, lo);
+
+            var last = new ActionBlock<string>(x => Console.WriteLine(x), edbo);
+
+            // this is preferred:
+            //second.LinkTo(third);
+
+            third.LinkTo(last, lo);
+
+            await first.SendAsync(5);
+            await first.SendAsync(1);
+
+            first.Complete();
+
+            await last.Completion;
+        }
+    }
+}
